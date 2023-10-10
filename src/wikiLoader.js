c41cceb445e666ee34f80411530b709a75d855b2
Elumenix <dps5393@g.rit.edu>
2023-10-10 14:26:53 -0400

    set up a way to find the best path between two wikipedia pages

--- src/wikiLoader.js
+++ src/wikiLoader.js
@@ -1,4 +1,63 @@
-const getRandomPage = (request, response) => {
+const state = {
+  sourcePageTitle: '',
+  targetPageTitle: '',
+  paths: [],
+  error: null,
+};
+
+
+const getShortestPath = () => {
+  return new Promise((resolve, reject) => {
+    // Test route
+    state.sourcePageTitle = 'Danilo Lim';
+    state.targetPageTitle = 'Usili Formation';
+
+    // Using an online websites api that can find the path between wikipedia pages
+    fetch('https://api.sixdegreesofwikipedia.com/paths', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        source: state.sourcePageTitle,
+        target: state.targetPageTitle,
+      }),
+    }).then((response) => response.json()).then((data) => {
+      // Creating new variables via destructuring
+      const {
+        pages,
+        paths,
+        //sourcePageTitle,
+        //targetPageTitle,
+        //isSourceRedirected,
+        //isTargetRedirected,
+      } = data;
+
+      // pathsDenormalized is an array of paths instead of pageIds
+      // Importantly, this lets me get the title and url of each page to give to the user
+      const pathsDenormalized = paths.map((path) => path.map((pageId) => pages[pageId]));
+
+      state.paths = pathsDenormalized;
+
+      // This pathway was successful
+      state.error = null;
+
+      // Return promise
+      resolve();
+    }).catch((error) => {
+      state.error = error;
+      console.log(state.error);
+
+      // reject the promise
+      reject(state.error);
+    });
+  })
+};
+
+
+const getRandomPage = async (request, response) => {
+  await getShortestPath();
+
   fetch('https://en.wikipedia.org/api/rest_v1/page/random/summary').then((recieved) => {
     if (recieved.ok) {
       return recieved.json();
@@ -6,10 +65,14 @@ const getRandomPage = (request, response) => {
 
     throw new Error('API request failed');
   }).then((data) => {
-    //console.log(data);
+    // console.log(data);
     response.writeHead(200, { 'Content-Type': request.headers.accept });
 
-    let returnData = JSON.stringify(data);
+    const returnData = JSON.stringify({
+      data: data,
+      state: state
+    });
+
     //console.log(returnData);
 
     response.write(returnData);
@@ -17,5 +80,6 @@ const getRandomPage = (request, response) => {
     return response;
   });
 };
+
 
 module.exports.getRandomPage = getRandomPage;
